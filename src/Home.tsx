7072a1dd948e296f20ac99f9a970258fe9d36b3f
chithraksha16 <chithrakshakharvi@gmail.com>
2026-01-05 18:36:30 +0530

    updated min height

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-      <section className='bg-[url(./maravanthe.jpeg)] bg-cover bg-center w-full h-[70vh]'>
+      <section className='bg-[url(./maravanthe.jpeg)] bg-cover bg-center w-full min-h-[50vh] md:min-h-[60vh] lg:min-h-[70vh]'>
 
       </section>
     </>
